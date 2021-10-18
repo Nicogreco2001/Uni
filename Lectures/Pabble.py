69bce9f942f08621b1a1bcebe22b35b862b7ac5a
Nicola Greco <Niccogreek@gmail.com>
2021-10-18 18:45:15 +0200

    Pabble game

---
+++ Lectures/Pabble.py
@@ -0,0 +1,11 @@
+game = [int(input("How many stones the first stack has?")),int(input("How many stones the second stack has?"))]
+if (game[0] % 2) != 0 and (game[1] % 2) != 0:
+    print("Remove one from both")
+    game[0] = game[0]-1
+    game[1] = game[1]-1
+    print(game + "This is a winning situation")
+if (game[0] % 2) == 0 and (game[1] % 2) == 0:
+    print("This is a lose situation")
+if (game[0] % 2) == 0 and (game[1] % 2) != 0:
+    
+
