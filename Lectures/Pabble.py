64d202a001f50a3e717fba5db3ba56dfcb94d976
Nicola Greco <Niccogreek@gmail.com>
2021-10-19 09:38:03 +0200

    lectures 18 oct

--- Lectures/Pabble.py
+++ Lectures/Pabble.py
@@ -3,9 +3,14 @@ if (game[0] % 2) != 0 and (game[1] % 2) != 0:
     print("Remove one from both")
     game[0] = game[0]-1
     game[1] = game[1]-1
-    print(game + "This is a winning situation")
+    print(game, "This is a winning situation")
 if (game[0] % 2) == 0 and (game[1] % 2) == 0:
     print("This is a lose situation")
 if (game[0] % 2) == 0 and (game[1] % 2) != 0:
-    
-
+    print("Remove one from the right pabble")
+    game[1] = game[1]-1
+    print(game, "This is a winning situation")
+if (game[0] % 2) != 0 and (game[1] % 2) == 0:
+    print("Remove one from the left pabble")
+    game[0] = game[0]-1
+    print(game, "This is a winning situation")
