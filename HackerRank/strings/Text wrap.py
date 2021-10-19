7f0b013063c4927c34af760c901d1e07f2c5e83e
Nicola Greco <Niccogreek@gmail.com>
2021-10-19 15:37:23 +0200

    Text Wrap Finished

--- HackerRank/strings/Text wrap.py
+++ HackerRank/strings/Text wrap.py
@@ -1,11 +1,13 @@
-string = "abcdefghij"
-max_width = 3
-for i in range(1,5):
-    if i == 1:
-        f = i * max_width
-        string = string[:f] + "\n" + string[f:]
-    else:
-        f = i * (max_width+1)
-        string = string[:f] + "\n" + string[f:]
+import textwrap
 
-print(string)
+def wrap(string, max_width):
+    a = (int(len(string)/max_width))
+    for i in range(1,a+1):
+        f = (i * (max_width)) + (i-1)
+        string = string[:f] + "\n" + string[f:]
+    return string
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
