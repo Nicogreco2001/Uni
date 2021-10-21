9f3179e94858aab95ee3e177a2e56baae96b0e70
Nicola Greco <Niccogreek@gmail.com>
2021-10-21 15:47:38 +0200

    t

--- Lectures/FibonacciFor.py
+++ Lectures/FibonacciFor.py
@@ -1,15 +1,11 @@
-def fibo(n):
-    if n == 0:
+def fibo_1_2(n):
+    cache = {}
+    if n in cache:
+        return cache[n]
+    if n == 1:
+        return 1
+    elif n == 0:
         return 0
-    elif n == 1:
-        return
     else:
-        fibo = 0 + 1
-        n_2, n_1 = 1, 1
-        for value in range(3,n+1):
-            fibo = n_2 + n_1
-            n_2 = n_1
-            n_1 = fibo
-        return fibo
-
-print((fibo(int((input())))))
+        return fibo_1_2(n-1) + fibo_1_2(n-2)
+print((fibo_1_2(int((input())))))
