72e22ca5053538ae1c1ac205b7e58ebcebb7be20
Nicola Greco <niccogreek@gmail.com>
2021-11-03 21:48:03 +0100

    c

--- HackerRank/collections/Solve Me First.py
+++ HackerRank/collections/Solve Me First.py
@@ -1,21 +1,22 @@
 from collections import defaultdict
-n, m = input().split()
-n, m = int(n), int(m)
+n, m = map(int, input().split())
 A = []
 B = []
 for a in range(n):
     A.append(input())
 for b in range(m):
     B.append(input())
-
-for b in B:
-    sol = defaultdict(list)
-    count = 0
-    while count < len(A):
-        if b == A[count]:
-            sol[b].append(count)
-
-for i in sol:
-    print(i)
+sol = defaultdict(list)
 
 
+
+for i in range(n):
+    sol[A[i]].append(i+1)
+
+
+for i in B:
+    if i in sol:
+        print(*sol[i])
+    else:
+        print(-1)
+        
