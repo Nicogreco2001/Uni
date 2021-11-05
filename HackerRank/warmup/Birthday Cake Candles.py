9ad492bf3c84d2785b7a087da64d204c4dc60779
Nicola Greco <niccogreek@gmail.com>
2021-11-05 22:04:03 +0100

    ff

---
+++ HackerRank/warmup/Birthday Cake Candles.py
@@ -0,0 +1,13 @@
+def birthdayCakeCandles(candles):
+    max = 0
+    count = 1
+    for a in candles:
+        if a > max:
+            count = 1
+            max = a
+        elif a == max:
+            count += 1
+    return count
+
+
+e
