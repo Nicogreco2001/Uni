d119cb8ab856a64b2e86a43dcd4b934851c4633e
Nicola Greco <niccogreek@gmail.com>
2022-03-13 12:44:53 +0100

    LONG

--- Rosalind/SUBS.py
+++ Rosalind/SUBS.py
@@ -5,8 +5,8 @@ m = len(sub)
 n = 0
 
 while n < (len(str)-m+1):
-    r_frame = str[n:n+m]# I set the reading frame equal to len of substring
-    if r_frame == sub:#when i found a substring i immediatly save position in the list
-        list.append(n+1)#n + 1 cause python gives index starting from 0
+    r_frame = str[n:n+m]   # I set the reading frame equal to len of substring
+    if r_frame == sub:     #when i found a substring i immediatly save position in the list
+        list.append(n+1)   #n + 1 cause python gives index starting from 0
     n += 1
 print(*list)
